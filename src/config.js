cca4f73ad7a8e3ef3a3e09ef85f10693cce7e4e9
Misha Kushka <kushka.misha@gmail.com>
2025-06-16 15:03:57 +0100

    Update rebalance frequency hours to weekly

--- src/config.js
+++ src/config.js
@@ -7,13 +7,13 @@ module.exports = {
     name: process.env.PORTFOLIO_NAME || "Main Portfolio",
     chainId: Number(process.env.CHAIN_ID) || 42161, // Arbitrum One
     maxRiskScore: parseFloat(process.env.MAX_RISK_SCORE) || 3.75,
-    rebalanceFrequencyHours: Number(process.env.REBALANCE_FREQUENCY_HOURS) || 1,
+    rebalanceFrequencyHours:
+      Number(process.env.REBALANCE_FREQUENCY_HOURS) || 24 * 7, // weekly
     serverUrl: process.env.SERVER_URL,
     minNumPositions: Number(process.env.MIN_NUM_POSITIONS) || 3,
     maxNumPositions: Number(process.env.MAX_NUM_POSITIONS) || 3,
-    initialAmountInUSD:
-      process.env.INITIAL_AMOUNT_IN_USD 
-      ? Number(process.env.INITIAL_AMOUNT_IN_USD) 
+    initialAmountInUSD: process.env.INITIAL_AMOUNT_IN_USD
+      ? Number(process.env.INITIAL_AMOUNT_IN_USD)
       : undefined,
     mainAssetAddr:
       process.env.MAIN_ASSET_ADDR ||
